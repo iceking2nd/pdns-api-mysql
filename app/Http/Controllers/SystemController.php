1408bcf8f7bc9f0885d9a65537aeae93da51e9bc
吴晓晨 <wuxiaochen@btte.net>
2018-04-09 17:00:16 +0800

    add database backup method

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -15,6 +15,6 @@ class SystemController extends APIController
         $db_name = env('DB_DATABASE');
 
         $command = sprintf('mysqldump -h\'%s\' -P\'%s\' -u\'%s\' -p\'%s\' %s', $db_host, $db_port, $db_user, $db_pass, $db_name);
-        dd(shell_exec($command));
+        return $this->response->accepted(null,json_encode(['data' => shell_exec($command)]));
     }
 }
